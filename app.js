ee1e4e769d48cdbee94f9f01a48c5a20414d23c6
Rishabh Sharma <76724111+rishabh0540@users.noreply.github.com>
2021-09-21 10:51:44 +0530

    Update app.js
    added commented line to connect to local or remote mongoDB server

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static("public"));
 
-// mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true});
+//uncomment below line if you want to connect to a local mongoDB database and replace <DBname> with the database name you want to use
+// mongoose.connect('mongodb://localhost:27017/<DBname>', {useNewUrlParser: true, useUnifiedTopology: true});   
+
+//uncomment below line if you want to connect to a remote mongoDB database and replace <user> with userID, <password> with password and <DBname> with the database-name
+// mongoose.connect("mongodb+srv://<user>:<password>@cluster0.kn7zr.mongodb.net/<DBname>?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get("/", function(req, res){
     //body
